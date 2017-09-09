c02fef4b1863904a0baf192300a4b6ec5fe2d53e
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-09 14:36:11 +0200

    Adds image uploader to new artist form

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,8 @@
 class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
 
+  mount_uploader :image_url, ImageUploader
+
   validates :name,  presence: true, length: { maximum: 50 }
   validates :image_url, presence: true
 
