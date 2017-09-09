d14a574b47b873af9905f58fa8f9e7424f2104e5
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-09 13:54:51 +0200

    Adds form to create new artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,6 +6,28 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+    @songs = @artist.songs
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist), notice: "Artist created!"
+    else
+      render :new
+    end
+  end
+
+private
+
+   def artist_params
+    params
+      .require(:artist)
+      .permit(:name, :image_url, song_ids: [])
   end
 
 end
