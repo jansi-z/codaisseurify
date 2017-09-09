4a552fdf42396245e76803d735ce2ee1b90cb0a3
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-09 11:22:28 +0200

    Adds model validation test for songs

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+
+    it "is invalid without a name" do
+      song = Song.new(name: "")
+      song.valid?
+      expect(song.errors).to have_key(:name)
+    end
+
+    it "is invalid with a name exceeding 50 characters" do
+      song = Song.new(name: "L" * 51)
+      song.valid?
+      expect(song.errors).to have_key(:name)
+    end
+
+  end
 end
