2c168fa4cb170c6790c79549ca4bfedb20ffa63c
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-10 18:13:36 +0200

    Adds shoulda for a validation test

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -4,11 +4,7 @@ RSpec.describe Song, type: :model do
 
   describe "validations" do
 
-    it "is invalid without a name" do
-      song = Song.new(name: "")
-      song.valid?
-      expect(song.errors).to have_key(:name)
-    end
+    it { should validate_presence_of(:name) }
 
     it "is invalid with a name exceeding 50 characters" do
       song = Song.new(name: "L" * 51)
