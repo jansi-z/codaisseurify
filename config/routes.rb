ae44a02596536c09e2ce718c23502c2d51f95904
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-10 17:21:52 +0200

    Slight tweaks to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "artists#index"
 
   resources :artists do
-    resources :songs
+    resources :songs, only: [:show, :new, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
