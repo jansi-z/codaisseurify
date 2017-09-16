d11d249a4c944af9944bdf002921855b08980fc6
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-16 11:34:32 +0200

    Adds namespace in routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:show, :new, :create, :destroy]
   end
+
+  namespace :api do
+    resources :artists do
+      resources :songs, only: [:show, :create, :destroy]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
