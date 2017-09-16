f0c8c58b805ce7d294497554f036d1e5078207ad
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-16 11:58:45 +0200

    Adds create action via api for songs

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,7 +1,34 @@
 class Api::SongsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def index
     render status: 200, json: {
       songs: Artist.find(params[:artist_id]).songs.all
     }.to_json
   end
+
+  def create
+    song = Artist.find(params[:artist_id]).songs.new(song_params)
+
+    if song.save
+      render status: 201, json: {
+        message: "Song successfully created",
+        song: song
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: song.errors
+      }.to_json
+    end
+  end
+
+  private
+
+  def song_params
+    params
+      .require(:song)
+      .permit(:name)
+  end
+
 end
