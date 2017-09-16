ed96fee33e7998795b72c90bc55a0a3c8ffa3379
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-16 16:12:13 +0200

    Adds javascript file

---
+++ app/assets/javascripts/songs.js
@@ -0,0 +1,29 @@
+function nextSongId() {
+  return $(".song").size() + 1;
+}
+
+function createSong(name) {
+  var songId = "song-" + nextTodoId();
+
+  var listItem = $('<li class="song"></li>')
+    .attr('for', songId)
+    .html(name);
+
+  $("#songList").append( listItem );
+}
+
+function submitSong(event) {
+  event.preventDefault();
+  createSong($("#song_name").val());
+  $("#song_name").val(null);
+}
+
+function deleteAllSongs(event) {
+  event.preventDefault();
+  $.when($("#song").remove())
+}
+
+$(document).ready(function() {
+  $("form").bind('submit', submitSong);
+  $("#clean-up").bind('click', deleteAllSongs);
+});
